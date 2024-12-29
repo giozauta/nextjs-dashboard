309ef9649c15b5f5804e9baa7e260df0dda38d48
giorgi <zautashvili1994@gmail.com>
2024-12-29 04:20:18 +0400

    16 chapter

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import { Metadata } from 'next';
+
+
+ 
+export const metadata: Metadata = {
+  title: 'Cusgtomers',
+};
+
 export default function Customers() {
     return <p>Customers Page</p>;
   }
--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,14 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
 import NotFound from './not-found';
+
+import { Metadata } from 'next';
+
+
+ 
+export const metadata: Metadata = {
+  title: 'Invoices | Edit Invoice',
+};
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
